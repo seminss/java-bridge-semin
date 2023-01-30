117849e1f6809c7cbad9f283b43aa34d6c39c865
seminss <2007ksm@naver.com>
2023-01-30 21:17:55 +0900

    testcase 성공

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,7 +6,6 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-//에러가 일어난 부분부터 다시 입력받기
 
     /**
      * 다리의 길이를 입력받는다.
@@ -33,7 +32,6 @@ public class InputView {
         while (true) {
             try{
                 String howMove = Console.readLine();
-                isString(howMove);
                 if (!howMove.equals("U") && !howMove.equals("D")) {
                     throw new IllegalArgumentException();
                 }
@@ -51,7 +49,6 @@ public class InputView {
         while (true) {
             try{
                 String replay = Console.readLine();
-                isString(replay);
                 if (!replay.equals("R") && !replay.equals("Q")) {
                     throw new IllegalArgumentException();
                 }
@@ -72,23 +69,9 @@ public class InputView {
                 int bridgeSize = Integer.parseInt(userInput);
                 return bridgeSize;
             } catch (Exception e) {
-                throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+                throw new IllegalArgumentException();
             }
         }
     }
 
-    /**
-     * 사용자 입력이 문자인지 판단한다.
-     */
-    //수정 필요
-    public void isString(String userInput) {
-        while (true) {
-            try {
-                Integer.parseInt(userInput);
-                System.out.println("[ERROR] 문자를 입력해 주세요.");
-            } catch (Exception e) { //String 이라면 에러나는게 정상
-                break;
-            }
-        }
-    }
 }
